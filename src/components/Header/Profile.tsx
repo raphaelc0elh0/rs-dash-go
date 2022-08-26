5d872ba65a128be8744d928e2e4fd9243b358a9a
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-26 15:50:10 -0300

    feat: responsiveness to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 
-export const Profile = () => {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export const Profile = ({ showProfileData = true }: ProfileProps) => {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Raphael Coelho</Text>
-        <Text color="gray.300" fontSize="small">
-          raphael_coelho@hotmail.com.br
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Raphael Coelho</Text>
+          <Text color="gray.300" fontSize="small">
+            raphael_coelho@hotmail.com.br
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Raphael Coelho" src="https://github.com/raphaelc0elh0.png" />
     </Flex>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon, Input, HStack, Text, Box, Avatar } from "@chakra-ui/react"
+import { Flex, Icon, Input, HStack, Text, Box, Avatar, useBreakpointValue } from "@chakra-ui/react"
 import { RiNotification2Line, RiSearch2Line, RiUser2Line } from "react-icons/ri"
 import { Logo } from "./Logo"
 import { NotificationsNav } from "./NotificationsNav"
@@ -6,13 +6,18 @@ import { Profile } from "./Profile"
 import { SearchBox } from "./SearchBox"
 
 export const Header = () => {
+  const isLargeVersion = useBreakpointValue<boolean>({
+    base: false,
+    lg: true
+  })
+
   return (
     <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
       <Logo />
-      <SearchBox />
+      {isLargeVersion && <SearchBox />}
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isLargeVersion} />
       </Flex>
     </Flex>
   )
