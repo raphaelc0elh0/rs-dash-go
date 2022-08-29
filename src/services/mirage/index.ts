1cdb5e82b10a60de069139298ebe7b8f4907c270
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-29 13:29:39 -0300

    build: miragejs, all set!

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,27 @@
+import { createServer, Model } from "miragejs"
+
+type User = {
+  name: string
+  email: string
+  createdAt: string
+}
+
+export const makeServer = () => {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+    routes() {
+      this.namespace = "api"
+      this.timing = 750
+
+      this.get("/users")
+      this.post("/users")
+
+      this.namespace = ""
+      this.passthrough()
+    }
+  })
+
+  return server
+}
